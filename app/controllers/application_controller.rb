a5dbd85dddc693ea5dc1ddc073ed15590bfe9bed
frank <franksavena@gmail.com>
2022-12-26 18:11:22 -0600

    commit tree d69154c8bee1d5c585cd1e48578e93f148515a7a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,26 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name], 
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+      )
+      baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    barkery = Bakery.find(params[:id])
+    barkery.update(name: params[:name])
+    barkery.to_json
+  end
+  
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
+
+
 end
